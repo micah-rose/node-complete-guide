0c32733b9aa9ed376ea1ab3036c6e0fae0b22c24
Micah Lund <micah.r.lund@gmail.com>
2020-08-17 21:34:46 -0600

    Moved response code into the event listener.

--- server-app/app.js
+++ server-app/app.js
@@ -30,11 +30,11 @@ const server = http.createServer((req, res) => {
             
             const message = parsedBody.split('=')[1];
             fs.writeFileSync('message.txt', message);
+            
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
