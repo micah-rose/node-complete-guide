2f153e2098e68d16683043d53d110e588d4cc115
Micah Lund <micah.r.lund@gmail.com>
2020-08-30 18:01:16 -0600

    Added use of middleware to understand how it works.

--- server-app/app.js
+++ server-app/app.js
@@ -2,6 +2,17 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); //allows request to move on to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    // ...
+});
+
 const server = http.createServer(app);
 
 server.listen(3002);
