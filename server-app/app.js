2706c9f888e2df9cce3681a568bf17a89db5b2dd
Micah Lund <micah.r.lund@gmail.com>
2020-09-02 19:09:01 -0600

    Added an example of how calling next works with middleware.

--- server-app/app.js
+++ server-app/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 
 //app.use([path,] callback [, path])
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+})
+
 app.use('/add-product', (req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>The "Add Product" Page</h1>'); //allows us to send a response and attach a body
