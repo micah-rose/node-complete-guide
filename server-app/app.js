f74ea165baa13a92a4f42f4e8b480b714670c1f1
Micah Lund <micah.r.lund@gmail.com>
2020-09-02 19:02:11 -0600

    Removed dummy middleware since it isn't doing anything.

--- server-app/app.js
+++ server-app/app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log('In the middleware');
-    next(); //allows request to move on to the next middleware in line
-});
-
-app.use((req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express!!</h1>'); //allows us to send a response and attach a body
 });
