90a1322c3130a0e21cf8549d086019a2c38abe68
Micah Lund <micah.r.lund@gmail.com>
2020-09-03 21:14:05 -0600

    Fixed a typo.

--- server-app/app.js
+++ server-app/app.js
@@ -10,7 +10,7 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button> type="submit">Add Product</button></form>'); //allows us to send a response and attach a body
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); //allows us to send a response and attach a body
 });
 
 app.use('/', (req, res, next) => {
