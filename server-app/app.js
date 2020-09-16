b0c8e6fdd9f55ab214b59dcdcbb7eabc43efd682
Micah Lund <micah.r.lund@gmail.com>
2020-09-16 17:16:36 -0600

    Removed reference comments as well as pug an hbs files since going forward we are using ejs for this project.

--- server-app/app.js
+++ server-app/app.js
@@ -3,21 +3,11 @@ const bodyParser = require('body-parser');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
-// const expressHbs = require('express-handlebars');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'server-app/views/layouts/', 
-//     defaultLayout: 'main-layout', 
-//     extname: 'hbs'}));
-// app.set('view engine', 'hbs');
-
-// app.set('view engine', 'pug');
-// app.set ('views', path.join(__dirname, 'views'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
@@ -26,7 +16,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
     res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
