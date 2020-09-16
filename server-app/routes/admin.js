b68c655995e2aefe4b3b15efc34d7ff23d2c9c86
Micah Lund <micah.r.lund@gmail.com>
2020-09-15 20:41:08 -0600

    Applied the same logic in admin.js

--- server-app/routes/admin.js
+++ server-app/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.use('/', (req, res, next) => {
     console.log('This always runs!');
@@ -7,8 +8,8 @@ router.use('/', (req, res, next) => {
 })
 
 router.get('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); 
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+ 
 });
 
 router.post('/add-product', (req, res, next) => {
