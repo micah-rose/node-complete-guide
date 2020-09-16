9f52bfdaa8c096c53a380e59a396ccc287bb4c6a
Micah Lund <micah.r.lund@gmail.com>
2020-09-15 20:20:28 -0600

    Modified folder structure.

--- server-app/app.js
+++ server-app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes-admin');
-const shopRoutes = require('./routes-shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
