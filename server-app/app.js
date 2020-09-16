68a223b88260ba1e22c11aa50028973867b526ee
Micah Lund <micah.r.lund@gmail.com>
2020-09-16 08:21:55 -0600

    Created an array to store data to make the app more dynamic.

--- server-app/app.js
+++ server-app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- server-app/routes/admin.js
+++ server-app/routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path');
 
+const products = [];
+
 router.use('/', (req, res, next) => {
     console.log('This always runs!');
     next();
@@ -14,8 +16,9 @@ router.get('/add-product', (req, res, next) => {
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
