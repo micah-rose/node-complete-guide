f826cb5b19aea09726b8ae5781a4d2308e9c1fdc
Micah Lund <micah.r.lund@gmail.com>
2020-09-15 20:08:47 -0600

    Went through an example of filtering paths using node.

--- server-app/routes-admin.js
+++ server-app/routes-admin.js
@@ -8,10 +8,10 @@ router.use('/', (req, res, next) => {
 
 router.get('/add-product', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); 
+    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); 
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
