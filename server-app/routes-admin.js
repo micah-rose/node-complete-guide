2c0227aa4b86597b6f2b72c9d2ae2675a2e0bc75
Micah Lund <micah.r.lund@gmail.com>
2020-09-15 20:09:41 -0600

    Adjusted path for get request.

--- server-app/routes-admin.js
+++ server-app/routes-admin.js
@@ -8,7 +8,7 @@ router.use('/', (req, res, next) => {
 
 router.get('/add-product', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); 
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'); 
 });
 
 router.post('/add-product', (req, res, next) => {
