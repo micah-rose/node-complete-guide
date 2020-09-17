aae3c787db55befda18ab401876da3bbecc7e13a
Micah Lund <micah.r.lund@gmail.com>
2020-09-17 09:50:47 -0600

    Moved get request in shop to products controller.

--- server-app/app.js
+++ server-app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- server-app/controllers/products.js
+++ server-app/controllers/products.js
@@ -11,3 +11,12 @@ exports.postAddProduct = (req, res, next) => {
     products.push({ title: req.body.title });
     res.redirect('/');
 }
+
+exports.getProducts = (req, res, next) => {
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/', 
+        hasProducts: products.length > 0,
+        activeShop: true});
+}
