2ee902e7c1598d05ed0db42589a04e2b1e019976
Micah Lund <micah.r.lund@gmail.com>
2020-09-17 09:32:05 -0600

    Created a controllers folder and moved get request in admin into a new file.

--- server-app/routes/admin.js
+++ server-app/routes/admin.js
@@ -1,16 +1,10 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products')
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {
-        pageTitle: 'Add Product', 
-        path: '/admin/add-product', 
-        activeAddProduct: true})
-});
+router.get('/add-product', productsController.getAddProduct);
 
 router.post('/add-product', (req, res, next) => {
     products.push({ title: req.body.title });
