475440080dec67ab2b2b0405c64e41abd5b7ea51
Micah Lund <micah.r.lund@gmail.com>
2020-09-17 20:19:43 -0600

    Adjusted controllers to be more specific to the files we are working with.

--- server-app/routes/shop.js
+++ server-app/routes/shop.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const productsController = require('../controllers/products')
+const productsController = require('../controllers/shop')
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
