c13fc7558a9ec824296e8d5c7d1ec9958914a24d
Micah Lund <micah.r.lund@gmail.com>
2020-09-17 20:13:02 -0600

    Added additional routes to shop.

--- server-app/routes/shop.js
+++ server-app/routes/shop.js
@@ -3,5 +3,8 @@ const productsController = require('../controllers/products')
 const router = express.Router();
 
 router.get('/', productsController.getProducts);
+router.get('/shop');
+router.get('/cart');
+router.get('/checkout');
 
 module.exports = router;
