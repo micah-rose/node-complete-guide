48f0405dc2923959bc54b67a792ebda59872ea16
Micah Lund <micah.r.lund@gmail.com>
2020-09-22 11:23:03 -0600

    Set up the logic for editing product information. For some reason I am getting an error on the JSON ending that the instructor isn't getting. Will need to keep an eye out for it if it seems to cause any issues.

--- server-app/controllers/admin.js
+++ server-app/controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 }
@@ -39,7 +39,22 @@ exports.getEditProduct = (req, res, next) => {
 }
 
 exports.postEditProduct = (req, res, next) => {
-    //postEditProduct
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedPrice = req.body.price;
+    const updatedDescr = req.body.description;
+
+    const updatedProduct = new Product(
+        prodId, 
+        updatedTitle,
+        updatedImageUrl,
+        updatedPrice,  
+        updatedDescr
+    );
+
+    updatedProduct.save();
+    res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) => {
