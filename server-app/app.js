d2a5151efd1e144d6b839a6db3241c9464db19ce
Micah Lund <micah.r.lund@gmail.com>
2020-09-25 17:21:31 -0600

    Removed code logic in product model since we will be rewriting it using the database.

--- server-app/app.js
+++ server-app/app.js
@@ -8,14 +8,6 @@ const db = require('./util/database');
 
 const app = express();
 
-db.execute('SELECT * FROM products')
-    .then(result => {
-        console.log(result[0], result[1]);
-    })
-    .catch(err => {
-        console.log(err);
-    });
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
