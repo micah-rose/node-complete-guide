4eb291594bfc13e6c964b4b3502ecc7da83d83b6
Micah Lund <micah.r.lund@gmail.com>
2020-09-24 23:55:34 -0600

    Began making a connection to sys database using MySQL.

--- server-app/app.js
+++ server-app/app.js
@@ -4,8 +4,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 const errorController = require('./controllers/error')
+const db = require('./util/database');
 
 const app = express();
+
+db.execute('SELECT * FROM products');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
