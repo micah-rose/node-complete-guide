1f3596ebcdf4fd052e157f18a7907a2044804128
Micah Lund <micah.r.lund@gmail.com>
2020-09-29 22:03:32 -0600

    Set up connection to the database using sequelize.

--- server-app/util/database.js
+++ server-app/util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'sys',
-    password: 'nodeSQL'
-});
+const sequelize = new Sequelize('sys', 'root', 'nodeSQL', {
+    dialect: 'mysql', 
+    host: 'localhost'
+})
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'sys',
+//     password: 'nodeSQL'
+// });
+
+// module.exports = pool.promise();
