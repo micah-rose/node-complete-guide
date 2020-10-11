bccc19dc970d48cbf027eadb41bfff994c8bfde7
Micah Lund <micah.r.lund@gmail.com>
2020-10-10 20:11:44 -0600

    Cleared some white space.

--- server-app/app.js
+++ server-app/app.js
@@ -8,7 +8,6 @@ const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
 
-
 const app = express();
 
 app.set('view engine', 'ejs');
