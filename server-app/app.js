e0fcadc4c056e2f45d85070524b3165a6aa68165
Micah Lund <micah.r.lund@gmail.com>
2020-10-13 16:42:18 -0600

    Created a new middleware to manage our dummy user.

--- server-app/app.js
+++ server-app/app.js
@@ -16,6 +16,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
