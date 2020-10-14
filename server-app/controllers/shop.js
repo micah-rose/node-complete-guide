31bc18adab2a4294ac8a2d7bc337d463b11fe11f
Micah Lund <micah.r.lund@gmail.com>
2020-10-14 16:59:17 -0600

    Began the process of adding new products into the cart. The database takes in a new cart each time the page loads, however. Checked the lecture notes and this will be corrected in a future lecture.

--- server-app/controllers/shop.js
+++ server-app/controllers/shop.js
@@ -60,10 +60,38 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts( {where: {id: prodId}} );
+    })
+    .then(products => {
+      let product;
+      if(products.length > 0){
+        const product = products[0];
+      }
+      if (product){
+        //Will apply this code later
+      }
+      return Product.findByPk(prodId)
+        .then(product => {
+          return fetchedCart.addProduct(product, { 
+            through: {quantity: newQuantity} 
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+  // const prodId = req.body.productId;
+  // Product.findById(prodId, product => {
+  //   Cart.addProduct(prodId, product.price);
+  // });
+  // res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
