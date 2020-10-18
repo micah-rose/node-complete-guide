fd2347c85c4a6d90deb01abea5b5084abf269921
Micah Lund <micah.r.lund@gmail.com>
2020-10-17 21:40:08 -0600

    Cleared the database,js file to take in a MongoDB connection.

--- server-app/util/database.js
+++ server-app/util/database.js
@@ -1,8 +1,1 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('sys', 'root', 'nodeSQL', {
-    dialect: 'mysql', 
-    host: 'localhost'
-})
-
-module.exports = sequelize;
+//Cleared for MongoDB connection
