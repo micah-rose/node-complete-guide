ea246cdfa669a9d170fe59f09ec063ca67de17e3
Micah Lund <micah.r.lund@gmail.com>
2020-10-19 21:58:07 -0600

    Finalized installation of MongoDB driver. Commented out sequelize routes to test connection and it works as expected.

--- server-app/app.js
+++ server-app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 const path = require('path');
 const errorController = require('./controllers/error')
 const mongoConnect = require('./util/database');
@@ -24,8 +24,8 @@ app.use((req, res, next) => {
     //     .catch(err => console.log(err));
 })
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
