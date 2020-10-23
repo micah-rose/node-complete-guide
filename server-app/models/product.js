bd2747b5fc665aa472792a6b1df882d26b23bc0f
Micah Lund <micah.r.lund@gmail.com>
2020-10-22 22:18:28 -0600

    Refactored the save function in the product model to be able to update a single product.

--- server-app/models/product.js
+++ server-app/models/product.js
@@ -2,24 +2,30 @@ const mongodb = require('mongodb');
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, imageUrl, price, description) {
+  constructor(title, imageUrl, price, description, id) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
+    this._id = id;
 
   }
 
   save() {
     const db = getDb();
-    return db.collection("products")
-      .insertOne(this)
-      .then(result => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    let dbOp;
+    if(this._id){
+      dbOp = db.collection("products").updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this});
+    } else {
+      dbOp = db.collection("products").insertOne(this)
+    }
+    return dbOp
+        .then(result => {
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   }
 
   static fetchAll() {
