6e4f29a6d826d4150b3ddaa86b4704543a9651b9
Micah Lund <micah.r.lund@gmail.com>
2020-10-22 21:44:05 -0600

    Adjusted the findById in the product model to compare _id to a new mongo object ID since it couldn't compare _id with a string.

--- server-app/models/product.js
+++ server-app/models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -38,7 +39,7 @@ class Product {
   static findById(prodId){
     const db = getDb();
     return db.collection("products")
-      .find({_id: prodId})
+      .find({_id: new mongodb.ObjectId(prodId)})
       .next()
       .then(product => {
         console.log(product);
