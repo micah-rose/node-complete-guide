e1c7ecd023974fd53c065989b99862937c2f94bc
Micah Lund <micah.r.lund@gmail.com>
2020-10-22 21:48:10 -0600

    Brought back the getProducts functionality from the admin controller and added the underscore in the products view so that the app will still work.

--- server-app/routes/admin.js
+++ server-app/routes/admin.js
@@ -3,7 +3,7 @@ const adminController = require('../controllers/admin')
 const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 
 router.post('/add-product', adminController.postAddProduct)
 // router.get('/edit-product/:productId', adminController.getEditProduct);
