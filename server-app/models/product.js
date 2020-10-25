3558ebe567bd8f4f6276bf589f1631496ea0c869
Micah Lund <micah.r.lund@gmail.com>
2020-10-24 19:06:50 -0600

    Created some spacing in the product model for easier code reading.

--- server-app/models/product.js
+++ server-app/models/product.js
@@ -13,11 +13,13 @@ class Product {
   save() {
     const db = getDb();
     let dbOp;
+
     if(this._id){
       dbOp = db.collection("products").updateOne({_id: this._id}, {$set: this});
     } else {
       dbOp = db.collection("products").insertOne(this)
     }
+
     return dbOp
         .then(result => {
           console.log(result);
@@ -29,6 +31,7 @@ class Product {
 
   static fetchAll() {
     const db = getDb();
+
     return db.collection("products")
       .find()
       .toArray()
@@ -43,6 +46,7 @@ class Product {
 
   static findById(prodId){
     const db = getDb();
+
     return db.collection("products")
       .find({_id: new mongodb.ObjectId(prodId)})
       .next()
@@ -57,6 +61,7 @@ class Product {
 
   static deleteById(prodId){
     const db = getDb();
+    
     return db.collection('products')
       .deleteOne({_id: new mongodb.ObjectId(prodId)})
       .then(result => {
