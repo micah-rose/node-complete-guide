fdfb8fc7325bba2d534b69ca506d797c99ce0820
Micah Lund <micah.r.lund@gmail.com>
2020-10-27 20:31:49 -0600

    Worked on storing multiple products in the cart.

--- server-app/models/user.js
+++ server-app/models/user.js
@@ -16,10 +16,25 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex(cp => {
-        //     return cp._id === product._id;
-        // });
-        const updatedCart = {items: [{productId: new ObjectId(product._id), quantity: 1}]};
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId == product._id;
+        });
+
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex > 0) {
+            newQuantity = this.cart.items[cartProductIndex] + 1;
+            updateCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({productId: new ObjectId(product._id), 
+                quantity: newQuantity})
+        }
+
+        const updatedCart = {
+            items: updatedCart 
+        };
+
         const db = getDb();
         return db.collection('users').updateOne(
             {_id: new ObjectId(this._id)},
