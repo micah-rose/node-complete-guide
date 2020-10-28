e8182dde5a6c7d55eb40e78b948eb3c0a0b18c7a
Micah Lund <micah.r.lund@gmail.com>
2020-10-27 22:23:29 -0600

    Added relational order data.

--- server-app/models/user.js
+++ server-app/models/user.js
@@ -75,15 +75,31 @@ class User {
 
     addOrder() {
         const db = getDb();
-        return db.collection('orders').insertOne(this.cart)
-            .then(result => {
-                this.cart = {items: []};
-                return db.collection('users').updateOne(
-                    {_id: new ObjectId(this._id)},
-                    {$set: {cart: {items: []}}}
-                );
-            })
+        return this.getCart().then(products => {
+            const order = {
+                items: products,
+                user: {
+                    _id: new ObjectId(this._id),
+                    name: this.name,
+                    email: this.email 
+                }
+            }
+            return db.collection('orders').insertOne(order);
+        })
+        .then(result => {
+            this.cart = {items: []};
+            return db.collection('users').updateOne(
+                {_id: new ObjectId(this._id)},
+                {$set: {cart: {items: []}}}
+            );
+        })
     }
+
+    // getOrders() {
+    //     const db = getDb();
+    //     return db.collection('orders')
+    //         .then()
+    // }
 
     static findById(userId){
         const db = getDb();
