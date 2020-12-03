b7a195fda40729a1bab65aa19d1d1d736344c825
Micah Lund <micah.r.lund@gmail.com>
2020-12-03 14:33:33 -0700

    Modified shop controller and cart view to populate and load the cart items.

--- server-app/models/user.js
+++ server-app/models/user.js
@@ -25,6 +25,8 @@ const userSchema = new Schema({
   }
 });
 
+
+
 userSchema.methods.addToCart = function(product) {
   const cartProductIndex = this.cart.items.findIndex(cp => {
     return cp.productId.toString() === product._id.toString();
