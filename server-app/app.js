b84fcef360e638d4d3151602909f6f6c9f857758
Micah Lund <micah.r.lund@gmail.com>
2020-12-03 20:28:18 -0700

    Started an auth routes file and registered it to app.js.

--- server-app/app.js
+++ server-app/app.js
@@ -4,6 +4,8 @@ const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
 
 const errorController = require("./controllers/error");
 const mongoose = require('mongoose');
@@ -28,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
