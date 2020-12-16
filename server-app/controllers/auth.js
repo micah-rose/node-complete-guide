055108679f096d3dd7ba01eea5b048dc30ecafe8
Micah Lund <micah.r.lund@gmail.com>
2020-12-15 20:03:56 -0700

    Finished sign in functionality.

--- server-app/controllers/auth.js
+++ server-app/controllers/auth.js
@@ -18,16 +18,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5fc9475ea278713c0cddca22")
-    .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.redirect("/login");
+    }
+    bcrypt
+      .compare(password, user.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
+        res.redirect("/login");
+      })
+      .catch((err) => {
         console.log(err);
-        res.redirect("/");
+        res.redirect("/login");
       });
-    })
-    .catch((err) => console.log(err));
+  });
 };
 
 exports.postSignup = (req, res, next) => {
