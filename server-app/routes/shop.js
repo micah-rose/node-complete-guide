ad0e2d96e2c3de5077d4c5c21475b9f0d2240342
Micah Lund <micah.r.lund@gmail.com>
2020-12-16 18:54:33 -0700

    Set up additional route protection by throwing the logic into a custom middleware to be reused throughout the app.

--- server-app/routes/shop.js
+++ server-app/routes/shop.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const shopController = require("../controllers/shop");
 const router = express.Router();
+const isAuth = require('../middleware/is-auth');
 
 router.get("/", shopController.getIndex);
 router.get("/shop", shopController.getProducts);
 router.get("/shop/:productId", shopController.getProductId);
-router.get("/cart", shopController.getCart);
-router.post("/cart", shopController.postCart);
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
-router.post("/create-order", shopController.postOrder);
-router.get("/orders", shopController.getOrders);
+router.get("/cart", isAuth, shopController.getCart);
+router.post("/cart", isAuth, shopController.postCart);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
+router.post("/create-order", isAuth, shopController.postOrder);
+router.get("/orders", isAuth, shopController.getOrders);
 
 module.exports = router;
