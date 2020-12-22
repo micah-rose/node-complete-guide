53eba5bfc6f61d246f954ae609b2e9e2812afb6e
Micah Lund <micah.r.lund@gmail.com>
2020-12-22 15:05:18 -0700

    Added status 422 to the postLogin action.

--- server-app/controllers/auth.js
+++ server-app/controllers/auth.js
@@ -45,6 +45,16 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email }).then((user) => {
     if (!user) {
       req.flash("error", "Invalid email or password.");
