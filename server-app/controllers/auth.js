49bfc7770e20a7e5836837d64ad2e1b6baa793c2
Micah Lund <micah.r.lund@gmail.com>
2020-12-21 22:54:00 -0700

    Set up validations for errors.

--- server-app/controllers/auth.js
+++ server-app/controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendGrid = require("nodemailer-sendgrid-transport");
 const crypto = require("crypto");
+const { validationResult } = require("express-validator");
 
 const transporter = nodemailer.createTransport(
   sendGrid({
@@ -74,6 +75,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array(),
+    });
+  }
 
   User.findOne({ email: email })
     .then((userDoc) => {
