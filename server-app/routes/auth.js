8f64b1a2673a6c72a4b34550e6fa93084c7c7619
Micah Lund <micah.r.lund@gmail.com>
2020-12-22 16:34:32 -0700

    Discovered the issue with the red border in the next lecture. I didn't have the validation set up in the auth routes for postLogin.

--- server-app/routes/auth.js
+++ server-app/routes/auth.js
@@ -6,23 +6,29 @@ const User = require("../models/user");
 
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
-router.post("/login", authController.postLogin);
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Please enter a valid email address."),
+    body("password", "Must have valid password.")
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 router.post(
   "/signup",
   [
     check("email")
-        .isEmail()
-        .withMessage("Please enter a valid email.")
-        .custom((value, { req }) => {
-            return User.findOne({ email: value })
-            .then((userDoc) => {
-              if (userDoc) {
-                return Promise.reject(
-                    'Email already exists.'
-                );
-              }
-            });
-    }),
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("Email already exists.");
+          }
+        });
+      }),
     body(
       "password",
       "Please enter a password with only numbers and text that is at least 5 characters."
@@ -34,7 +40,7 @@ router.post(
         throw new Error("Passwords have to match.");
       }
       return true;
-    })
+    }),
   ],
   authController.postSignup
 );
