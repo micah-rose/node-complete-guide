56320c7c2895adaec2d2eb6a82ac1a59fe915e97
Micah Lund <micah.r.lund@gmail.com>
2020-12-22 16:03:19 -0700

    Attempted to finalize validation to the login form, but the red border for incorrect data isn't working. Going to run through the lecture again to see what is missing.

--- server-app/controllers/auth.js
+++ server-app/controllers/auth.js
@@ -25,6 +25,11 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+    },
+    validationErrors: [],
   });
 };
 
@@ -53,18 +58,30 @@ exports.postLogin = (req, res, next) => {
   const password = req.body.password;
 
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     return res.status(422).render("auth/login", {
       path: "/login",
       pageTitle: "Login",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+      },
+      validationErrors: errors.array(),
     });
   }
   User.findOne({ email: email }).then((user) => {
     if (!user) {
-      req.flash("error", "Invalid email or password.");
-      return res.redirect("/login");
+      return res.status(422).render("auth/login", {
+        path: "/login",
+        pageTitle: "Login",
+        errorMessage: "Invalid email or password.",
+        oldInput: {
+          email: email,
+          password: password,
+        },
+        validationErrors: [],
+      });
     }
     bcrypt
       .compare(password, user.password)
@@ -77,14 +94,23 @@ exports.postLogin = (req, res, next) => {
             res.redirect("/");
           });
         }
-        req.flash("error", "Invalid email or password.");
-        res.redirect("/login");
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password.",
+          oldInput: {
+            email: email,
+            password: password,
+          },
+          validationErrors: [],
+        });
       })
       .catch((err) => {
         console.log(err);
         res.redirect("/login");
       });
-  });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
