b719f97a4afd75e288fe3d65c7a79d881512ec07
Micah Lund <micah.r.lund@gmail.com>
2020-12-21 23:05:18 -0700

    Finalized error validation to validate the email.

--- server-app/controllers/auth.js
+++ server-app/controllers/auth.js
@@ -78,10 +78,11 @@ exports.postSignup = (req, res, next) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    console.log(errors.array());
     return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
-      errorMessage: errors.array(),
+      errorMessage: errors.array()[0].msg,
     });
   }
 
