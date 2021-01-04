37bb9aafd0c270e761ef822a42ddf3bebef5fdca
Micah Lund <micah.r.lund@gmail.com>
2021-01-03 18:16:12 -0700

    Generated a custom PDF file using the order data.

--- server-app/controllers/shop.js
+++ server-app/controllers/shop.js
@@ -1,8 +1,9 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
-const fs = require('fs');
-const path = require('path');
-const PDFDocument = require('pdfkit');
+const fs = require("fs");
+const path = require("path");
+const PDFDocument = require("pdfkit");
+const { profile } = require("console");
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -10,7 +11,7 @@ exports.getProducts = (req, res, next) => {
       res.render("shop/shop", {
         prods: products,
         pageTitle: "All Products",
-        path: "/products"
+        path: "/products",
       });
     })
     .catch((err) => {
@@ -27,7 +28,7 @@ exports.getProductId = (req, res, next) => {
       res.render("shop/product-details", {
         product: product,
         pageTitle: product.title,
-        path: "/products"
+        path: "/products",
       });
     })
     .catch((err) => {
@@ -43,7 +44,7 @@ exports.getIndex = (req, res, next) => {
       res.render("shop/index", {
         prods: products,
         pageTitle: "Shop",
-        path: "/"
+        path: "/",
       });
     })
     .catch((err) => {
@@ -62,7 +63,7 @@ exports.getCart = (req, res, next) => {
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
-        products: products
+        products: products,
       });
     })
     .catch((err) => {
@@ -140,7 +141,7 @@ exports.getOrders = (req, res, next) => {
       res.render("shop/orders", {
         path: "/orders",
         pageTitle: "Your Orders",
-        orders: orders
+        orders: orders,
       });
     })
     .catch((err) => {
@@ -152,23 +153,53 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
-  Order.findById(orderId).then(order => {
-    if (!order) {
-      return next(new Error('No order found!'));
-    }
-    if (order.user.userId.toString() !== req.user._id.toString()){
-      return next(new Error('Unauthorized!'));
-    } else {
-    const invoiceName = 'INVOICE-' + orderId + '.pdf';
-    const invoicePath = path.join('server-app', 'data', 'invoices', invoiceName);
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found!"));
+      }
+      if (order.user.userId.toString() !== req.user._id.toString()) {
+        return next(new Error("Unauthorized!"));
+      } else {
+        const invoiceName = "INVOICE-" + orderId + ".pdf";
+        const invoicePath = path.join(
+          "server-app",
+          "data",
+          "invoices",
+          invoiceName
+        );
 
-    const pdfDoc = new PDFDocument();
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName +'"');
-    pdfDoc.pipe(fs.createWriteStream(invoicePath));
-    pdfDoc.pipe(res);
-    pdfDoc.text('Hello World');
-    pdfDoc.end();
-  }
-  }).catch(err => console.log(err))
-}
+        const pdfDoc = new PDFDocument();
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+          "Content-Disposition",
+          'inline; filename="' + invoiceName + '"'
+        );
+        pdfDoc.pipe(fs.createWriteStream(invoicePath));
+        pdfDoc.pipe(res);
+
+        pdfDoc.fontSize(26).text("Invoice", {
+          underline: true,
+        });
+        pdfDoc.text("-----------------------------------------------------");
+
+        let totalPrice = 0;
+        order.products.forEach((prod) => {
+          totalPrice += prod.quantity * prod.product.price;
+          pdfDoc.fontSize(14).text(
+            prod.product.title +
+              " - " +
+              prod.quantity +
+              " x " +
+              "$" +
+              prod.product.price
+          );
+        });
+        pdfDoc.text("-----------------------------");
+        pdfDoc.fontSize(18).text('Total Price: $' + totalPrice);
+
+        pdfDoc.end();
+      }
+    })
+    .catch((err) => console.log(err));
+};
