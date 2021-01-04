72fe2a50138241e3cc545d4a77102b3d951b9816
Micah Lund <micah.r.lund@gmail.com>
2021-01-03 18:00:12 -0700

    Set up the project to stream the data versus statically serve the data.

--- server-app/controllers/shop.js
+++ server-app/controllers/shop.js
@@ -161,15 +161,19 @@ exports.getInvoice = (req, res, next) => {
     const invoiceName = 'INVOICE-' + orderId + '.pdf';
     const invoicePath = path.join('server-app', 'data', 'invoices', invoiceName);
   
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        console.log(err);
-        return next(err);
-      }
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName +'"');
-      res.send(data);
-    });
+    // fs.readFile(invoicePath, (err, data) => {
+    //   if (err) {
+    //     console.log(err);
+    //     return next(err);
+    //   }
+    //   res.setHeader('Content-Type', 'application/pdf');
+    //   res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName +'"');
+    //   res.send(data);
+    // });
+    const file = fs.createReadStream(invoicePath);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName +'"');
+    file.pipe(res);
   }
   }).catch(err => console.log(err))
 }
