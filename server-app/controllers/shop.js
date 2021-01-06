1eb08d56d50f80c5a8253317696827744cd4d434
Micah Lund <micah.r.lund@gmail.com>
2021-01-05 21:16:13 -0700

    Put the pagination in its own ejs file so it can be reused.

--- server-app/controllers/shop.js
+++ server-app/controllers/shop.js
@@ -7,14 +7,30 @@ const PDFDocument = require("pdfkit");
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
-    .then((products) => {
-      res.render("shop/shop", {
-        prods: products,
-        pageTitle: "All Products",
-        path: "/products",
-      });
-    })
+    const page = +req.query.page || 1;
+    let totalItems;
+  
+    Product.find()
+      .countDocuments()
+      .then((numProducts) => {
+        totalItems = numProducts;
+        return Product.find()
+          .skip((page - 1) * ITEMS_PER_PAGE)
+          .limit(ITEMS_PER_PAGE);
+      })
+      .then((products) => {
+        res.render("shop/shop", {
+          prods: products,
+          pageTitle: "All Products",
+          path: "/products",
+          currentPage: page,
+          hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+          hasPreviousPage: page > 1,
+          nextPage: page + 1,
+          previousPage: page - 1,
+          lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
+        });
+      })
     .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
